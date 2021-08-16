89bad95a5f5a9ed832996382650ef459f2e09575
Jiayin Li <jiayin.li007@outlook.com>
2021-08-16 17:17:29 -0400

    get Prettier installed

--- 03-Developer-Skills/starter/script.js
+++ 03-Developer-Skills/starter/script.js
@@ -1,3 +1,10 @@
 // Remember, we're gonna use strict mode in all scripts now!
-'use strict';
+"use strict";
 
+const x = 23;
+
+if (x === 23) console.log(x);
+
+const clacAge = birthYear => 2021 - birthYear;
+
+console.log(clacAge(1992));
