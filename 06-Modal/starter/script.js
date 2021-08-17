d4ca62b8a500262ff03d1f58d2551a8b79cb29e3
Jiayin Li <jiayin.li007@outlook.com>
2021-08-17 07:13:01 -0400

    finished Modal Window Functions(open/close)

--- 06-Modal/starter/script.js
+++ 06-Modal/starter/script.js
@@ -1,1 +1,33 @@
 'use strict';
+
+const modal = document.querySelector('.modal');
+const overlay = document.querySelector('.overlay');
+const btnCloseModal = document.querySelector('.close-modal');
+const btnsShowModal = document.querySelectorAll('.show-modal');
+
+// // only the first one will be selected
+// const btnsShowModal = document.querySelector('.show-modal');
+// console.log(btnsShowModal);
+
+console.log(btnsShowModal);
+
+// Separate openModal and closeModal as functions
+const openModal = function () {
+  // besides remove classes we can also add and check classes
+  modal.classList.remove('hidden'); // dot is not used here
+  overlay.classList.remove('hidden');
+};
+
+for (let i = 0; i < btnsShowModal.length; i++) {
+  //   console.log(btnsShowModal[i].textContent);
+  btnsShowModal[i].addEventListener('click', openModal);
+}
+
+const closeModal = function () {
+  modal.classList.add('hidden');
+  overlay.classList.add('hidden');
+};
+
+// Both click the close button and the outside range will make the modal close
+btnCloseModal.addEventListener('click', closeModal);
+overlay.addEventListener('click', closeModal);
