e94d1f19f6172d90b6142328ee9d53f0e86501f7
Jiayin Li <jiayin.li007@outlook.com>
2021-08-17 15:40:54 -0400

    completed Reset functionality

--- 07-Pig-Game/starter/script.js
+++ 07-Pig-Game/starter/script.js
@@ -17,19 +17,35 @@ const btnHold = document.querySelector('.btn--hold');
 // console.log(score0El);
 // console.log(score1El);
 
-// Initial settings
-score0El.textContent = 0;
-score1El.textContent = 0;
-diceEl.classList.add('hidden');
-
+// keeps track of whether continuing the game
+let playing;
 // keeps track of current active player
-let activePlayer = 0;
+let activePlayer;
 // keeps track of score for current active player
-let currentScore = 0;
+let currentScore;
 // keeps track of scores for both players as an array
 const scores = [0, 0];
-// keeps track of whether continuing the game
-let playing = true;
+
+const init = function () {
+  // Initial settings
+  score0El.textContent = 0;
+  score1El.textContent = 0;
+  diceEl.classList.add('hidden');
+
+  playing = true;
+  activePlayer = 0;
+
+  current0El.textContent = 0;
+  current1El.textContent = 0;
+  scores[0] = 0;
+  scores[1] = 0;
+  currentScore = 0;
+
+  player0El.classList.remove('player--winner');
+  player1El.classList.remove('player--winner');
+  player0El.classList.add('player--active');
+  player1El.classList.remove('player--active');
+};
 
 const switchPlayer = function () {
   document.getElementById(`current--${activePlayer}`).textContent = 0;
@@ -42,7 +58,6 @@ const switchPlayer = function () {
   player1El.classList.toggle('player--active');
 };
 
-//TODO: Rolling dice functionality
 btnRoll.addEventListener('click', function () {
   if (playing) {
     // 1. Generating a random dice roll
@@ -60,8 +75,6 @@ btnRoll.addEventListener('click', function () {
       // current0El.textContent = currentScore;
       document.getElementById(`current--${activePlayer}`).textContent =
         currentScore;
-
-      // FIXME: reset background color when switch
     } else {
       switchPlayer();
     }
@@ -91,3 +104,7 @@ btnHold.addEventListener('click', function () {
     }
   }
 });
+
+btnNew.addEventListener('click', init);
+
+init();
